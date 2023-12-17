21d455139c0b8e918c7e31c53de582e04be1583d
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2023-12-17 18:11:46 +0100

    123

---
+++ e103/pointclouds/index/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1441254,
+    "boundingBox": {
+        "lx": 104098.90400025623,
+        "ly": 1211523.568999744,
+        "lz": -0.10400014166037508,
+        "ux": 104149.95399949329,
+        "uy": 1211574.618998981,
+        "uz": 50.94599909540017
+    },
+    "tightBoundingBox": {
+        "lx": 104098.90400025623,
+        "ly": 1211523.568999744,
+        "lz": -0.10400014166037508,
+        "ux": 104149.95399949329,
+        "uy": 1211562.2960009343,
+        "uz": 12.84600002523263
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.4421059489250183,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
